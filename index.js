901de080ce5d0aea7c62737fc7bcc929e0401331
Yesaya Jones <yesayajones@gmail.com>
2022-12-20 23:18:50 +0200

    feat: add function to display each book on page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let myLibrary = ['Paradise', 'Hell', 'Heaven'];
+let myLibrary = ['Paradise', 'Hell', 'Heaven', 'Mountain'];
+let container = document.querySelector('.container');
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -10,16 +11,16 @@ function Book(title, author, pages, read) {
 	};
 }
 
-// let theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 255, 'not');
-// console.log(theHobbit.info());
-
 function addBookToLibrary(book) {
 	myLibrary.push(book);
 }
 
 function loop() {
 	myLibrary.forEach((book) => {
-		console.log(book);
+		const book_card = document.createElement('div');
+		book_card.classList.add('card');
+		book_card.innerHTML = `${book}`;
+		container.appendChild(book_card);
 	});
 }
 
